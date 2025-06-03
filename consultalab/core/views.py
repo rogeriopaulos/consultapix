6688b5120ed0802cad3dbb27e2a71b1c128c9306
Rogerio Paulo <rogeriopaulos@gmail.com>
2025-06-03 17:59:42 -0300

    feat: Implementa paginação na lista de requisições e ajusta o layout do formulário na página inicial

--- consultalab/core/views.py
+++ consultalab/core/views.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
+from django.core.paginator import Paginator
 from django.views.generic import TemplateView
 
 from consultalab.bacen.filters import RequisicaoBacenFilter
@@ -12,13 +15,27 @@ class HomeView(LoginRequiredMixin, TemplateView):
     """
 
     template_name = "pages/home.html"
+    page_size = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         qs = RequisicaoBacen.objects.filter(user=self.request.user).order_by("-created")
-        filter = RequisicaoBacenFilter(self.request.GET, queryset=qs)
-        context["requisicoes_filter"] = filter
-        filter.form.helper = RequisicaoBacenFilterFormHelper()
+        requisicao_filter = RequisicaoBacenFilter(self.request.GET, queryset=qs)
+
+        requisicao_filter.form.helper = RequisicaoBacenFilterFormHelper()
+
+        paginator = Paginator(requisicao_filter.qs, self.page_size)
+        page_number = self.request.GET.get("page")
+        try:
+            paginated_objects = paginator.page(page_number)
+        except PageNotAnInteger:
+            paginated_objects = paginator.page(1)
+        except EmptyPage:
+            paginated_objects = paginator.page(paginator.num_pages)
+
+        context["requisicoes_filter"] = requisicao_filter
+        context["requisicoes"] = paginated_objects
+
         return context
 
 
