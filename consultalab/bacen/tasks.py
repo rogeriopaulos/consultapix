6f8c6d2c1ffb3e2d650ec619262507d1501b8833
Rogério Paulo <rogeriopaulos@gmail.com>
2025-06-06 13:00:35 -0300

    feat: Atualiza tarefa Celery para buscar informações de PIX por CPF ou CNPJ com tratamento de erros e registro de logs

--- consultalab/bacen/tasks.py
+++ consultalab/bacen/tasks.py
@@ -1,17 +1,37 @@
 import logging
-import time
 
 from celery import shared_task
+
+from consultalab.bacen.helpers import BacenRequestApi
+from consultalab.bacen.models import RequisicaoBacen  # ChavePix, EventoVinculo
 
 logger = logging.getLogger(__name__)
 
 
 @shared_task(name="request_pix_by_cpfcnpj")
-def request_pix_by_cpfcnpj(cpf):
-    logger.info("Iniciando tarefa de busca de PIX por CPF/CNPJ")
-    time.sleep(10)
+def request_pix_by_cpfcnpj(value: str, reason: str, requisicao_id: int) -> dict:
+    """
+    Tarefa Celery para buscar informações de PIX por CPF ou CNPJ.
+    """
+    if not value:
+        logger.error("Valor vazio fornecido para busca de PIX por CPF/CNPJ")
+        return {
+            "status": "error",
+            "message": "Valor vazio fornecido para busca de PIX por CPF/CNPJ",
+        }
+
+    logger.info(f"Iniciando busca de PIX por CPF/CNPJ: {value}")
+    response = BacenRequestApi().request_pix_by_cpfcnpj(value, reason)
+    if not response.status == 200:
+        return {
+            "status": "error",
+            "message": response.message,
+        }
     logger.info("Tarefa de busca de PIX por CPF/CNPJ concluída")
+
+    requisicao = RequisicaoBacen.objects.get(id=requisicao_id)
+
     return {
         "status": "success",
-        "message": "Busca de PIX por CPF/CNPJ concluída com sucesso",
+        "message": "Dados de busca de PIX por CPF/CNPJ salvos com sucesso",
     }
